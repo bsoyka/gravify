7a04fab15639dabbc1f3bf7ff6266e3418ab103c
Benjamin Soyka <bensoyka@icloud.com>
2019-10-02 07:16:08 -0700

    Update test_main.py

--- tests/test_main.py
+++ tests/test_main.py
@@ -3,40 +3,43 @@ import unittest
 
 sys.path.insert(0, '../')
 
-import gravify
+from gravify import Gravatar
 
 
 class TestMain(unittest.TestCase):
 
     def test_plain_email(self):
-        self.assertEqual(gravify.Gravatar("bensoyka@icloud.com").url,
+        self.assertEqual(Gravatar("bensoyka@icloud.com").url,
                          "https://www.gravatar.com/avatar/7246821a7bf0b1b37794b39cb08ee052")
 
     def test_hash(self):
-        self.assertEqual(gravify.Gravatar("bensoyka@icloud.com").hash, "7246821a7bf0b1b37794b39cb08ee052")
+        self.assertEqual(Gravatar("bensoyka@icloud.com").hash, "7246821a7bf0b1b37794b39cb08ee052")
 
     def test_hash_whitespace(self):
-        self.assertEqual(gravify.Gravatar("   \tbensoyka@icloud.com\n").hash, "7246821a7bf0b1b37794b39cb08ee052")
+        self.assertEqual(Gravatar("   \tbensoyka@icloud.com\n").hash, "7246821a7bf0b1b37794b39cb08ee052")
 
     def test_hash_to_lowercase(self):
-        self.assertEqual(gravify.Gravatar("bEnSoYkA@iClOuD.cOm").hash, "7246821a7bf0b1b37794b39cb08ee052")
+        self.assertEqual(Gravatar("bEnSoYkA@iClOuD.cOm").hash, "7246821a7bf0b1b37794b39cb08ee052")
 
     def test_default_identicon(self):
-        self.assertEqual(gravify.Gravatar("bensoyka@icloud.com", default_image="identicon").url,
+        self.assertEqual(Gravatar("bensoyka@icloud.com", default_image="identicon").url,
                          "https://www.gravatar.com/avatar/7246821a7bf0b1b37794b39cb08ee052?d=identicon")
 
     def test_without_param(self):
         self.assertEqual(
-            gravify.Gravatar("bensoyka@icloud.com").url,
+            Gravatar("bensoyka@icloud.com").url,
             "https://www.gravatar.com/avatar/7246821a7bf0b1b37794b39cb08ee052")
 
-    def test_with_size_param(self):
+    def test_size_param(self):
         self.assertEqual(
-            gravify.Gravatar("bensoyka@icloud.com", size=200).url,
+            Gravatar("bensoyka@icloud.com", size=200).url,
             "https://www.gravatar.com/avatar/7246821a7bf0b1b37794b39cb08ee052?s=200")
-    
+        self.assertEqual(
+            Gravatar("bensoyka@icloud.com", size=300).url,
+            "https://www.gravatar.com/avatar/7246821a7bf0b1b37794b39cb08ee052?s=300")
+
     def test_file_handler(self):
-        self.assertIsInstance(gravify.Gravatar("bensoyka@icloud.com").file.read(4), bytes)
+        self.assertIsInstance(Gravatar("bensoyka@icloud.com").file.read(4), bytes)
 
 
 if __name__ == "__main__":
