44f648a5af13a0c2d57739ae17f3ed088ffe3b8a
Benjamin Soyka <bensoyka@icloud.com>
2019-10-01 19:46:28 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="gravify",
-    version="0.2.0",
+    version="0.3.0",
     author="Benjamin Soyka",
     author_email="bensoyka@icloud.com",
     description="A simple package to generate a Gravatar URL",
