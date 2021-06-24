98b20e3b83f5d3600b0ea250b967b6c99826ccf1
Ben Soyka <bensoyka@icloud.com>
2021-06-23 22:03:36 -0600

    Switch to pytest and format

--- gravify/__init__.py
+++ gravify/__init__.py
@@ -5,13 +5,23 @@ import urllib.request
 import sentry_sdk
 from validate_email import validate_email
 
-sentry_sdk.init("https://d1a3441649e64a96b52c441233c47f26@sentry.io/1763873")  # Automatic bug reporting
+sentry_sdk.init(
+    'https://d1a3441649e64a96b52c441233c47f26@sentry.io/1763873'
+)  # Automatic bug reporting
 
 
-class Gravatar():
-    def __init__(self, email, verify_email=True, default_image=None, size=None, force_default=False, max_rating=None):
+class Gravatar:
+    def __init__(
+        self,
+        email,
+        verify_email=True,
+        default_image=None,
+        size=None,
+        force_default=False,
+        max_rating=None,
+    ):
         if verify_email and not validate_email(email):
-            raise Exception("Invalid email address")
+            raise Exception('Invalid email address')
 
         self.email = email.strip().lower()
         self.default_image = default_image
@@ -22,37 +32,42 @@ class Gravatar():
     @property
     def url(self):
         params = {
-            "s": self.size,
-            "d": self.default_image,
-            "f": "y" if self.force_default else None,
-            "r": self.max_rating if self.max_rating in ["g", "pg", "r", "x"] else None
+            's': self.size,
+            'd': self.default_image,
+            'f': 'y' if self.force_default else None,
+            'r': self.max_rating
+            if self.max_rating in ['g', 'pg', 'r', 'x']
+            else None,
         }
 
-        url_params = urllib.parse.urlencode({k: v for k, v in params.items() if v is not None})
+        url_params = urllib.parse.urlencode(
+            {k: v for k, v in params.items() if v is not None}
+        )
 
-        _url = f"https://www.gravatar.com/avatar/{self.hash}"
+        _url = f'https://www.gravatar.com/avatar/{self.hash}'
 
-        return f"{_url}?{url_params}" if url_params else _url
+        return f'{_url}?{url_params}' if url_params else _url
 
     @property
     def unsecure_url(self):
-        return self.url.replace("https://", "http://")
+        return self.url.replace('https://', 'http://')
 
     @property
     def hash(self):
-        return hashlib.md5(self.email.encode("utf-8")).hexdigest()
+        return hashlib.md5(self.email.encode('utf-8')).hexdigest()
 
     @property
     def file(self):
         return urllib.request.urlopen(self.url)
 
+
 DEFAULT_AVATARS = {
-        '404': '404',
-        'blank': 'blank',
-        'identicon': 'identicon',
-        'monsterid': 'monsterid',
-        'mystery_person': 'mp',
-        'retro': 'retro',
-        'robohash': 'robohash',
-        'wavatar': 'wavatar',
-    }
+    '404': '404',
+    'blank': 'blank',
+    'identicon': 'identicon',
+    'monsterid': 'monsterid',
+    'mystery_person': 'mp',
+    'retro': 'retro',
+    'robohash': 'robohash',
+    'wavatar': 'wavatar',
+}
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,60 +1,87 @@
 from gravify import Gravatar, DEFAULT_AVATARS
-import sys
-import unittest
-
-sys.path.insert(0, '../')
 
 
-class TestMain(unittest.TestCase):
-
-    def test_plain_email(self):
-        self.assertEqual(Gravatar("bensoyka@icloud.com").url,
-                         "https://www.gravatar.com/avatar/7246821a7bf0b1b37794b39cb08ee052")
-
-    def test_hash(self):
-        self.assertEqual(Gravatar("bensoyka@icloud.com").hash,
-                         "7246821a7bf0b1b37794b39cb08ee052")
-
-    def test_hash_whitespace(self):
-        self.assertEqual(Gravatar("   \tbensoyka@icloud.com\n").hash,
-                         "7246821a7bf0b1b37794b39cb08ee052")
-
-    def test_hash_to_lowercase(self):
-        self.assertEqual(Gravatar("bEnSoYkA@iClOuD.cOm").hash,
-                         "7246821a7bf0b1b37794b39cb08ee052")
-
-    def test_default_identicon(self):
-        self.assertEqual(Gravatar("bensoyka@icloud.com", default_image="identicon").url,
-                         "https://www.gravatar.com/avatar/7246821a7bf0b1b37794b39cb08ee052?d=identicon")
-
-    def test_default_image_constant(self):
-        self.assertEqual(Gravatar("bensoyka@icloud.com", default_image=DEFAULT_AVATARS['identicon']).url,
-                         "https://www.gravatar.com/avatar/7246821a7bf0b1b37794b39cb08ee052?d=identicon")
-
-    def test_size_param(self):
-        self.assertEqual(Gravatar("bensoyka@icloud.com", size=200).url,
-                         "https://www.gravatar.com/avatar/7246821a7bf0b1b37794b39cb08ee052?s=200")
-        self.assertEqual(Gravatar("bensoyka@icloud.com", size=300).url,
-                         "https://www.gravatar.com/avatar/7246821a7bf0b1b37794b39cb08ee052?s=300")
-
-    def test_force_default_param(self):
-        self.assertEqual(Gravatar("bensoyka@icloud.com", force_default=True).url,
-                         "https://www.gravatar.com/avatar/7246821a7bf0b1b37794b39cb08ee052?f=y")
-
-    def test_file_handler(self):
-        self.assertIsInstance(
-            Gravatar("bensoyka@icloud.com").file.read(4), bytes)
-
-    def test_ignore_unknown_rating(self):
-        self.assertEqual(Gravatar("bensoyka@icloud.com", max_rating='xyz').url,
-                         "https://www.gravatar.com/avatar/7246821a7bf0b1b37794b39cb08ee052")
-
-    def test_max_rating(self):
-        self.assertEqual(Gravatar("bensoyka@icloud.com", max_rating='g').url,
-                         "https://www.gravatar.com/avatar/7246821a7bf0b1b37794b39cb08ee052?r=g")
-        self.assertEqual(Gravatar("bensoyka@icloud.com", default_image="identicon", max_rating='g').url,
-                         "https://www.gravatar.com/avatar/7246821a7bf0b1b37794b39cb08ee052?d=identicon&r=g")
+def test_plain_email():
+    assert (
+        Gravatar('bensoyka@icloud.com').url
+        == 'https://www.gravatar.com/avatar/7246821a7bf0b1b37794b39cb08ee052'
+    )
 
 
-if __name__ == "__main__":
-    unittest.main()
+def test_hash():
+    assert (
+        Gravatar('bensoyka@icloud.com').hash
+        == '7246821a7bf0b1b37794b39cb08ee052'
+    )
+
+
+def test_hash_whitespace():
+    assert (
+        Gravatar('   \tbensoyka@icloud.com\n').hash
+        == '7246821a7bf0b1b37794b39cb08ee052'
+    )
+
+
+def test_hash_to_lowercase():
+    assert (
+        Gravatar('bEnSoYkA@iClOuD.cOm').hash
+        == '7246821a7bf0b1b37794b39cb08ee052'
+    )
+
+
+def test_default_identicon():
+    assert (
+        Gravatar('bensoyka@icloud.com', default_image='identicon').url
+        == 'https://www.gravatar.com/avatar/7246821a7bf0b1b37794b39cb08ee052?d=identicon'
+    )
+
+
+def test_default_image_constant():
+    assert (
+        Gravatar(
+            'bensoyka@icloud.com', default_image=DEFAULT_AVATARS['identicon']
+        ).url
+        == 'https://www.gravatar.com/avatar/7246821a7bf0b1b37794b39cb08ee052?d=identicon'
+    )
+
+
+def test_size_param():
+    assert (
+        Gravatar('bensoyka@icloud.com', size=200).url
+        == 'https://www.gravatar.com/avatar/7246821a7bf0b1b37794b39cb08ee052?s=200'
+    )
+    assert (
+        Gravatar('bensoyka@icloud.com', size=300).url
+        == 'https://www.gravatar.com/avatar/7246821a7bf0b1b37794b39cb08ee052?s=300'
+    )
+
+
+def test_force_default_param():
+    assert (
+        Gravatar('bensoyka@icloud.com', force_default=True).url
+        == 'https://www.gravatar.com/avatar/7246821a7bf0b1b37794b39cb08ee052?f=y'
+    )
+
+
+def test_file_handler():
+    assert isinstance(Gravatar('bensoyka@icloud.com').file.read(4), bytes)
+
+
+def test_ignore_unknown_rating():
+    assert (
+        Gravatar('bensoyka@icloud.com', max_rating='xyz').url
+        == 'https://www.gravatar.com/avatar/7246821a7bf0b1b37794b39cb08ee052'
+    )
+
+
+def test_max_rating():
+    assert (
+        Gravatar('bensoyka@icloud.com', max_rating='g').url
+        == 'https://www.gravatar.com/avatar/7246821a7bf0b1b37794b39cb08ee052?r=g'
+    )
+    assert (
+        Gravatar(
+            'bensoyka@icloud.com', default_image='identicon', max_rating='g'
+        ).url
+        == 'https://www.gravatar.com/avatar/7246821a7bf0b1b37794b39cb08ee052?d=identicon&r=g'
+    )
