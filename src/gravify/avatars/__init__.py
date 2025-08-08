6cbeb358a44de908d4c3a8062e94d162e00bd666
Ben Soyka <hello@bsoyka.me>
2025-08-07 21:58:43 -0600

    chore: better expose needed models

--- src/gravify/avatars/__init__.py
+++ src/gravify/avatars/__init__.py
@@ -1,7 +1,9 @@
 """Profile-related functionality for Gravatar."""
 
-from gravify.avatars.generator import AvatarGenerator
+from gravify.avatars.generator import AvatarGenerator, DefaultImage, Rating
 
 __all__ = [
     'AvatarGenerator',
+    'DefaultImage',
+    'Rating',
 ]
