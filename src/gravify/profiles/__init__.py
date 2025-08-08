1ae16b8d4a1ee5b8c51a58df626e2498c2a8bc10
Ben Soyka <hello@bsoyka.me>
2025-08-07 21:58:43 -0600

    chore: better expose needed models

--- src/gravify/profiles/__init__.py
+++ src/gravify/profiles/__init__.py
@@ -1,7 +1,9 @@
 """Profile-related functionality for Gravatar."""
 
 from gravify.profiles.api import ProfileAPI
+from gravify.profiles.models import Profile
 
 __all__ = [
+    'Profile',
     'ProfileAPI',
 ]
