360e3e71bc7991f6ffe8bbadc176341775a3b0dc
Ben Soyka <hello@bsoyka.me>
2025-08-07 19:05:36 -0600

    feat(api): add GravatarAPI client for profile fetching

--- src/gravify/__init__.py
+++ src/gravify/__init__.py
@@ -2,4 +2,11 @@
 
 import importlib.metadata
 
+from gravify.gravatar_api import GravatarAPI
+
 __version__ = importlib.metadata.version('gravify')
+
+__all__ = [
+    'GravatarAPI',
+    '__version__',
+]
